77ef18d6205461e6321a46d09f20f059e901bd2a
krishnabhagwat11 <krishna.heaven204@gmail.com>
2026-02-03 17:35:34 +0530

    new

--- src/app/station-list/page.js
+++ src/app/station-list/page.js
@@ -1,5 +1,10 @@
+import { Suspense } from 'react';
 import StationList from '@/tab/station-list/StationList';
 
 export default function StationListPage() {
-  return <StationList />;
+  return (
+    <Suspense fallback={null}>
+      <StationList />
+    </Suspense>
+  );
 }
